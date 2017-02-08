cf1c284de153f035b7726d2231939e9b5d1ed6b4
tyson <tyson.funk@smail.rasmussen.edu>
2017-02-07 19:08:45 -0800

    Solution Rename

--- App1/BL_PageContent.cs
+++ App1/BL_PageContent.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 
 
-namespace App1
+namespace TysonFunkApp
 {
     class BL_PageContent
     {
--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -17,7 +17,7 @@ using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace App1
+namespace TysonFunkApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
